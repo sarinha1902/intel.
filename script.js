7a25bfd79f6ab9da938b58a833a2fb5f27873441
sara gabriely <111572637+sarinha1902@users.noreply.github.com>
2024-05-23 08:15:19 -0300

    att

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector(".caixa-prinipal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector("caixa-resultado");
+const caixaPrincipal = document.querySelector(".caixa-prinipal");
