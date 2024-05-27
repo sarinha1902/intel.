95b3eb41d7c288503429363ec0c0b886b3b6b716
sara gabriely <111572637+sarinha1902@users.noreply.github.com>
2024-05-27 09:06:08 -0300

    att

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const caixaPrincipal = document.querySelector(".caixa-prinipal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector("caixa-resultado");
-const caixaPrincipal = document.querySelector(".caixa-prinipal");
+const caixaResultado= document.querySelector(".caixa-resultado");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+
+const perguntas = ["Pergunta 1", "Pergunta 2"]
